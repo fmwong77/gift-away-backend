b461a0eb9b60f03af7b27b86bd83c7b41497983b
fmwong77 <fongmeiw@gmail.com>
2020-02-28 11:02:45 -0600

    added image in post_serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -6,15 +6,15 @@ class PostSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   attributes :id, :title, :description, :user_id, :category_id, :latitude, :longitude
 
-  # def image
-  #   return unless object.image.attached?
-  #   object.image.blob.attributes
-  #         .slice('filename', 'byte_size')
-  #         .merge(url: image_url)
-  #         .tap { |attrs| attrs['name'] = attrs.delete('filename') }
-  # end
+  def image
+    return unless object.image.attached?
+    object.image.blob.attributes
+          .slice('filename', 'byte_size')
+          .merge(url: image_url)
+          .tap { |attrs| attrs['name'] = attrs.delete('filename') }
+  end
 
-  # def image_url
-  #   url_for(object.image)
-  # end
+  def image_url
+    url_for(object.image)
+  end
 end
