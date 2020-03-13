774cf3e109263008fe0acd9fb3f636490a2918af
fmwong77 <fongmeiw@gmail.com>
2020-03-12 19:21:48 -0500

    remove migration files

--- db/migrate/20200225203337_add_image_to_post.rb
+++
@@ -1,8 +0,0 @@
-class AddImageToPost < ActiveRecord::Migration[6.0]
-  def self.up
-      add_attachment :posts, :image
-   end
-   def self.down
-      remove_attachment :posts, :image
-   end
-end
